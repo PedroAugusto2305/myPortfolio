d32eda7eb88a5a92f6f1c7b42b6578b8c8067616
Pedro Augusto <p.augusto_23@outlook.com>
2022-08-08 17:44:23 -0300

    Creating New Pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-document.querySelectorAll('a').forEach(link => {
-  const content = document.getElementById('#content')
+// document.querySelectorAll('a').forEach(link => {
+//   const content = document.getElementById('#content')
 
-  link.onclick = function (e) {
-    e.preventDefault()
-    fetch(link.getAttribute('wm-nav'))
-      .then(resp => resp.text())
-      .then(html => (content.innerHTML = html))
-  }
-})
+//   link.onclick = function (e) {
+//     e.preventDefault()
+//     fetch(link.getAttribute('wm-nav'))
+//       .then(resp => resp.text())
+//       .then(html => (content.innerHTML = html))
+//   }
+// })
